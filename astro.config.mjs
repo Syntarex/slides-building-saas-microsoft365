4dabafea3d581b1f9cdb0c3c5c5f7fcc1147abfa
Syntarex <4340303+Syntarex@users.noreply.github.com>
2025-05-13 17:22:03 +0200

    Install daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import alpine from "@astrojs/alpinejs";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import https from "@vitejs/plugin-basic-ssl";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 /** @type {import('astro').AstroConfig} */
 export default defineConfig({
@@ -19,5 +19,23 @@ export default defineConfig({
         server: {
             https: import.meta.env.DEV, // Enables https for local development
         },
+    },
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Syncopate",
+                cssVariable: "--font-syncopate",
+                fallbacks: [
+                    "Segoe UI",
+                    "Segoe UI Web (West European)",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Helvetica Neue",
+                    "sans-serif",
+                ],
+                weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+            },
+        ],
     },
 });
